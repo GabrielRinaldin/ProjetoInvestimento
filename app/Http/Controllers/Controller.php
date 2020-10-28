df7c76a4a35e9029f2c6ea48b640f289619d1cbf
GabrielRinaldin <gabrielrinaldin@hotmail.com>
2020-10-28 12:02:01 -0300

    dia3

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -19,7 +20,8 @@ class Controller extends BaseController
 
     public function cadastrar()
     {
-       return view('auth.register');
+       //return view('auth.register');
+       echo "Cadastrar";
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+  
 Auth::routes();
 Route::get('/', 'App\Http\Controllers\Controller@homepage');
 Route::get('cadastro', 'App\Http\Controllers\Controller@cadastrar');
-
 
 /**
  * Routes to user auth
  * ==========================================
  */
+
 Route::get('login', 'App\Http\Controllers\Controller@fazerlogin');
 Route::post('login', ['as' => 'user.login', 'uses' => 'App\Http\Controllers\DashboardController@auth']);
+Route::get('dashboard', ['as' => 'user.dashboard', 'uses' => 'App\Http\Controllers\DashboardController@index']);
+
+
